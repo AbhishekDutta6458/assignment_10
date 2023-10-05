054169c3f949fb6f3bfeebe0ac76dd624cc40e4d
abhishekdutta6458 <abhishekdutta835@gmail.com>
2023-10-06 01:29:27 +0530

    Add lab 10

--- lab/9/README.md
+++
@@ -1,6 +0,0 @@
-```bash
-  npm create vite@latest 9 -- --template react
-  cd ./9
-  npm install
-  npm run dev
-```
--- lecture/filepicker.js
+++
@@ -1,7 +0,0 @@
-function filePicker() {
-  const fp = document.getElementById("fp");
-  const separator_at = fp.files[0].name.lastIndexOf(".");
-  if (separator_at != -1) {
-    alert(fp.files[0].name.slice(separator_at));
-  }
-}
